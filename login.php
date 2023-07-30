c39fd721f9a7c6646ec0e4f2182c9f2924e645d0
AloisGoeury <alois.goeury@mines-albi.fr>
2023-07-31 09:31:38 +1000

    show errors after login and gitignore

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
-    require_once 'config.php';
-    session_start();
+require_once 'config.php';
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Vérifier si l'utilisateur est déjà connecté
 if (isset($_SESSION['username'])) {
